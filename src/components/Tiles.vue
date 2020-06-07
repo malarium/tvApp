53b1be45ae6ae1f84023b1f34dfc096d9603555c
Marcin <marcingorecki1981@gmail.com>
2020-06-07 19:56:44 +0200

    scrollable modal

--- src/components/Tiles.vue
+++ src/components/Tiles.vue
@@ -13,11 +13,11 @@
       >
         <b-card-text>
           <p>Premiered: {{ show.premiered }}</p>
-          <p>Avg rating: {{ show.rating.average }}</p>
+          <p>Avg rating: {{ show.rating.average || 'none' }}</p>
         </b-card-text>
       </b-card>
     </div>
-    <b-modal v-if="modalData" size="xl" ref="my-modal" title hide-footer>
+    <b-modal v-if="modalData" size="xl" ref="my-modal" title hide-footer scrollable>
       <div class="container-fluid">
         <div class="row">
           <div class="col-12 col-md-6">
@@ -30,7 +30,7 @@
                 class="align-self-center showRating"
                 :data-rating="modalData.rating.average"
                 :class="classObject()"
-              >Rating: {{ modalData.rating.average }}</span>
+              >Rating: {{ modalData.rating.average || 'none' }}</span>
             </div>
             <div class="d-flex flex-row justify-content-between">
               <p class="align-self-center">Premiere: {{ modalData.premiered }}</p>
@@ -48,6 +48,57 @@
               :href="'https://www.imdb.com/title/'+ modalData.externals.imdb"
               target="_blank"
             >Zobacz na IMDB</a>
+
+            <div role="tablist">
+              <b-card no-body class="mb-1">
+                <b-card-header
+                  header-tag="header"
+                  class="p-1"
+                  role="tab"
+                  @click="getCastInfo($event, modalData.id)"
+                >
+                  <b-button block v-b-toggle.accordion-1 variant="info">CAST</b-button>
+                </b-card-header>
+                <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
+                  <b-card-body>
+                    <b-card-text v-for="castSingle of castInfo" :key="castSingle.id">
+                      <div class="row">
+                        <figure class="col-6">
+                          <img
+                            class="img-fluid"
+                            style="max-width:90px;"
+                            :src="castSingle.character.image.medium"
+                          />
+                          <figcaption>{{ castSingle.character.name }}</figcaption>
+                        </figure>
+                        <div class="col-6">
+                          <a
+                            :href="castSingle.person.url"
+                            target="_blank"
+                          >{{ castSingle.person.name }}</a>
+                        </div>
+                      </div>
+                    </b-card-text>
+                  </b-card-body>
+                </b-collapse>
+              </b-card>
+
+              <b-card no-body class="mb-1">
+                <b-card-header
+                  header-tag="header"
+                  class="p-1"
+                  role="tab"
+                  @click="getCrewInfo($event, modalData.id)"
+                >
+                  <b-button block v-b-toggle.accordion-2 variant="info">CREW</b-button>
+                </b-card-header>
+                <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+                  <b-card-body>
+                    <b-card-text>{{ crewInfo }}</b-card-text>
+                  </b-card-body>
+                </b-collapse>
+              </b-card>
+            </div>
           </div>
         </div>
       </div>
@@ -64,7 +115,9 @@ export default {
     return {
       singleResult: [],
       endpointQuery: "https://api.tvmaze.com/singlesearch/shows?q=",
-      modalData: null
+      modalData: null,
+      castInfo: null,
+      crewInfo: null
     };
   },
   methods: {
@@ -72,7 +125,9 @@ export default {
       console.log(this.singleResult);
     },
     classObject() {
-      if (this.modalData.rating.average < 5) {
+      if (this.modalData.rating.average === null) {
+        return "none";
+      } else if (this.modalData.rating.average < 5) {
         return "poor";
       } else if (
         this.modalData.rating.average > 5 &&
@@ -84,7 +139,7 @@ export default {
       }
     },
     showMore(id) {
-      return fetch("https://api.tvmaze.com/shows/" + id)
+      return fetch(`https://api.tvmaze.com/shows/${id}`)
         .then(blob => blob.json())
         .then(data => {
           this.modalData = data;
@@ -94,12 +149,33 @@ export default {
             this.$refs["my-modal"].show();
           });
         });
+    },
+    getCastInfo(e, id) {
+      this.castInfo = null;
+      if (e.target.classList.contains("not-collapsed")) {
+        return fetch(`https://api.tvmaze.com/shows/${id}/cast`)
+          .then(blob => blob.json())
+          .then(data => {
+            console.log(data);
+            this.castInfo = data;
+          });
+      }
+    },
+    getCrewInfo(e, id) {
+      this.crewInfo = null;
+      if (e.target.classList.contains("not-collapsed")) {
+        return fetch(`https://api.tvmaze.com/shows/${id}/crew`)
+          .then(blob => blob.json())
+          .then(data => {
+            console.log(data);
+            this.crewInfo = data;
+          });
+      }
     }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .card-whole {
   article div {
@@ -149,6 +225,11 @@ export default {
   &.good {
     &:before {
       color: green;
+    }
+  }
+  &.none {
+    &:before {
+      content: "\2606";
     }
   }
 }
