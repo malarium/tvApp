b702bea3e82a6b2a997a510c8671dcc9a610a6ca
Marcin <marcingorecki1981@gmail.com>
2020-06-08 10:42:27 +0200

    logs

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="tv app logo" class="logo" src="./assets/tv.png" />
-    <Heading />
-    <Search @searchCommenced="searchCommenced" />
-    <Tiles :shows="showsAll" />
-  </div>
+	<div id="app">
+		<img alt="tv app logo" class="logo" src="./assets/tv.png" />
+		<Heading />
+		<Search @searchCommenced="searchCommenced" />
+		<Tiles :shows="showsAll" />
+	</div>
 </template>
 
 <script>
-import Heading from "./components/Heading.vue";
-import Search from "./components/Search.vue";
-import Tiles from "./components/Tiles.vue";
+	import Heading from "./components/Heading.vue";
+	import Search from "./components/Search.vue";
+	import Tiles from "./components/Tiles.vue";
 
-export default {
-  name: "App",
-  components: {
-    Heading,
-    Search,
-    Tiles
-  },
-  data: () => {
-    return {
-      showsAll: [],
-      dataIncoming: false
-    };
-  },
-  methods: {
-    getShowsList() {
-      return fetch("https://api.tvmaze.com/shows")
-        .then(blob => blob.json())
-        .then(data => {
-          this.showsAll = data;
-        });
-    },
-    searchCommenced(dataIncoming) {
-      if (dataIncoming && dataIncoming.length > 0) {
-        this.showsAll = [];
-        this.showsAll = [...dataIncoming];
-        console.log(this.showsAll);
-      } else {
-        this.getShowsList();
-      }
-    }
-  },
-  beforeMount() {
-    this.getShowsList();
-  }
-};
+	export default {
+	  name: "App",
+	  components: {
+	    Heading,
+	    Search,
+	    Tiles
+	  },
+	  data: () => {
+	    return {
+	      showsAll: [],
+	      dataIncoming: false
+	    };
+	  },
+	  methods: {
+	    getShowsList() {
+	      return fetch("https://api.tvmaze.com/shows")
+	        .then(blob => blob.json())
+	        .then(data => {
+	          this.showsAll = data;
+	        });
+	    },
+	    searchCommenced(dataIncoming) {
+	      console.log('data incoming: ', dataIncoming)
+	      if (dataIncoming && dataIncoming.length > 0) {
+	        this.showsAll = [];
+	        this.showsAll = [...dataIncoming];
+	      } else {
+	        this.getShowsList();
+	      }
+	    }
+	  },
+	  beforeMount() {
+	    this.getShowsList();
+	  }
+	};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #f5c17a;
-  padding-top: 60px;
-  .logo {
-    width: 140px;
-  }
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		background-color: #f5c17a;
+		padding-top: 60px;
+		.logo {
+			width: 140px;
+		}
+	}
 </style>
